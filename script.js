eb9b677a0d142a9306fb0d0d93b4c92d8d0f187e
Lamangano <vmangano@gmail.com>
2022-05-08 12:29:44 -0300

    version 05

---
+++ script.js
@@ -0,0 +1,5 @@
+function lista (){
+alert("Alfredo Bustos, Armando Livani, Lucía Boero, Enrique Gibert Mella, Laura Cáceres, Cesar Tello, Raul Iriarte, Susana Cardonnet, Nora Plaza, Edgardo Matoso, Irene Abreu")}
+
+let Profes= document.getElementById("profes")
+Profes.addEventListener("click", lista)
