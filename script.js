daa4a0e688048cc829715ef798e31daf6f5efac3
Mac2020 <mac2020@MacBook-Air-de-Mac2020.local>
2022-05-18 11:16:46 -0300

    version08

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function lista (){
 alert("Alfredo Bustos, Armando Livani, Lucía Boero, Enrique Gibert Mella, Laura Cáceres, Cesar Tello, Raul Iriarte, Susana Cardonnet, Nora Plaza, Edgardo Matoso, Irene Abreu")}
 
-let Profes= document.getElementById("profes")
+let Profes = document.getElementById("profes")
 Profes.addEventListener("click", lista)
+
+
+
